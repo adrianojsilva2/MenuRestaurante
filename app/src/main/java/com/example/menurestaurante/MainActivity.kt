365ee629cd0037b242b0b2d3a05afc696eb7efa0
Adriano J Silva Vieira <adriano.vurmo@gmail.com>
2023-05-05 09:59:17 -0300

    Initial commit

---
+++ app/src/main/java/com/example/menurestaurante/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.menurestaurante
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.menurestaurante.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val precoPao = 0.5
+    private val precoCafe = 1.0
+    private val precoChocolate = 1.2
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonPedido.setOnClickListener {
+
+            val i = Intent(this, SplashScreenActivity::class.java)
+            i.putExtra("quant_cafe", binding.editQuantidadeCafe.text.toString())
+            i.putExtra("quant_pao", binding.editQuantidadePao.text.toString())
+            i.putExtra("quant_chocolate", binding.editQuantidadeChocolate.text.toString())
+            i.putExtra("preco_cafe", precoCafe)
+            i.putExtra("preco_pao", precoPao)
+            i.putExtra("preco_chocolate", precoChocolate)
+            startActivity(i)
+        }
+
+
+        binding.checkCafe.setOnClickListener {
+            if (binding.checkCafe.isChecked()){
+                binding.editQuantidadeCafe.setText("1")
+                binding.precoCafe.visibility = View.VISIBLE
+            } else{
+                binding.editQuantidadeCafe.setText("0")
+                binding.precoCafe.visibility = View.GONE
+            }
+        }
+
+
+        binding.checkChocolate.setOnClickListener {
+            if (binding.checkChocolate.isChecked()){
+                binding.editQuantidadeChocolate.setText("1")
+                binding.precoChocolate.visibility = View.VISIBLE
+            } else{
+                binding.editQuantidadeChocolate.setText("0")
+                binding.precoChocolate.visibility = View.GONE
+            }
+        }
+
+        binding.checkPao.setOnClickListener {
+            if (binding.checkPao.isChecked()){
+                binding.editQuantidadePao.setText("1")
+                binding.precoPao.visibility = View.VISIBLE
+            } else{
+                binding.editQuantidadePao.setText("0")
+                binding.precoPao.visibility = View.GONE
+            }
+        }
+    }
+}
